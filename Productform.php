9fcfb8d3e66e8087e41aaeee54b33306833d85b8
ShaqirTopko <99472564+ShaqirTopko@users.noreply.github.com>
2023-02-26 13:54:25 +0100

    Productform.php
    U shtua pjesa e Productform.php

---
+++ Productform.php
@@ -0,0 +1,26 @@
+<?php
+	include 'db_connect.php';
+	
+	$sql = "SELECT id, name, photo, price, createdBy, createdDate FROM products";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	  // output data of each row
+	  while($row = $result->fetch_assoc()) {
+        echo '
+		<div class="product" data-name="p-1">
+         <img src="../'.$row["photo"].'" alt="">
+         <h3>'.$row["name"].'</h3>
+         <div class="price">'.$row["price"].'</div>
+		</div> 
+		
+		';
+	  }
+	} else {
+		echo "0 results";
+	}
+	$conn->close();
+
+?>
+
+		
